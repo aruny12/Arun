b5499618858cfe37b1f088edb85473722d941751
aruny12 <aruny19901178@gmail.com>
2021-06-09 21:40:30 +0530

    adding src file

---
+++ src/test/java/com/simpleSeleniumProject/Test/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.simpleSeleniumProject.Test;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test1() {
+		System.out.println("Test1 method");
+	}
+}
